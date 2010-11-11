adff6278d35e7d32218fa7781776f7042d33db35
ivankartolo <ivankartolo@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-11 22:17:26 +0000

    init import TER version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_subscription/trunk@40094 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "direct_mail_subscription".
+#
+# Auto generated 11-11-2010 23:16
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Direct Mail Subscription',
+	'description' => 'Adds a plugin for subscription to direct mail newsletters (collecting subscriptions in the tt_address table)',
+	'category' => 'plugin',
+	'shy' => 1,
+	'version' => '1.1.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Kasper Skårhøj',
+	'author_email' => 'kasper@typo3.com',
+	'author_company' => 'Curby Soft Multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:12:"ext_icon.gif";s:4:"8d58";s:14:"ext_tables.php";s:4:"f6c1";s:28:"ext_typoscript_constants.txt";s:4:"3daf";s:24:"ext_typoscript_setup.txt";s:4:"86bb";s:13:"locallang.php";s:4:"1ab3";s:27:"pi/class.dmailsubscribe.php";s:4:"650e";s:30:"pi/fe_admin_dmailsubscrip.tmpl";s:4:"495f";s:33:"pi/fe_admin_dmailsubscrip_dk.tmpl";s:4:"862b";}',
+);
+
+?>
---
+++ locallang.php
@@ -0,0 +1,43 @@
+<?php
+$LOCAL_LANG = Array (
+	"default" => Array (
+		"pi_dmail_subscr" => "DirectMail subscription",
+	),
+	"dk" => Array (
+		"pi_dmail_subscr" => "Nyhedsbrevstilmelding",
+	),
+	"de" => Array (
+		"pi_dmail_subscr" => "Direct Mail Anmeldung",
+	),
+	"no" => Array (
+		"pi_dmail_subscr" => "Nyhetsbrevsinnmelding",
+	),
+	"it" => Array (
+		"pi_dmail_subscr" => "Sottoscrizione Direct Mail",
+	),
+	"fr" => Array (
+		"pi_dmail_subscr" => "Inscription DirectMail",
+	),
+	"es" => Array (
+		"pi_dmail_subscr" => "Suscripción al Correo Directo",
+	),
+	"nl" => Array (
+		"pi_dmail_subscr" => "DirectMail abonnement",
+	),
+	"cz" => Array (
+		"pi_dmail_subscr" => "Subscripce Direct mail",
+	),
+	"pl" => Array (
+		"pi_dmail_subscr" => "Subskrybcja",
+	),
+ 	"si" => Array (
+		"pi_dmail_subscr" => "DirectMail naroènina",
+	),
+	"fi" => Array (
+		"pi_dmail_subscr" => "Suora sähköposti tilaus",
+	),
+	"tr" => Array (
+		"pi_dmail_subscr" => "Doðrudan Mail abonmaný",
+	)
+);
+?>
