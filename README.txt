ec32d2bf16477355cafe9e0d9272bdee9c7d2b22
ivankartolo <ivankartolo@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-06 22:31:28 +0000

    Ticket #13264: moved typoscript to static template
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_subscription/trunk@52790 735d13b6-9817-0410-8766-e36946ffe9aa

--- README.txt
+++ README.txt
@@ -1,4 +1,5 @@
 Upon upgrading, please notes these changes:
+	- TypoScript is moved to static template. Please include this manually
 	- fe_adminLib.inc is now local. please check the path to this script in your TypoScript.
 	- new subpart in template ###TEMPLATE_INFOMAIL_NORECORD###
 	- added support for captcha. Please check the standard TypoScript in ext_typoscript_setup.txt
--- ext_tables.php
+++ ext_tables.php
@@ -19,6 +19,7 @@ t3lib_div::loadTCA("tt_address");
 t3lib_extMgm::addTCAcolumns("tt_address",$tempColumns,1);
 t3lib_extMgm::addToAllTCAtypes("tt_address","tx_directmailsubscription_localgender", '', 'after:gender');
 
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/','Direct Mail subscription');
 
 t3lib_extMgm::addPlugin(Array("LLL:EXT:direct_mail_subscription/locallang.php:pi_dmail_subscr", "21"));
 ?>
