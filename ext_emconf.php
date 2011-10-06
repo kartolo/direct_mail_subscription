442a8c9b13862aa0ee0a6e33043babd8009dc0b3
ivankartolo <ivankartolo@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-06 21:41:18 +0000

    emconf 1.1.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_subscription/trunk@52788 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "direct_mail_subscription".
 #
-# Auto generated 11-11-2010 23:16
+# Auto generated 06-10-2011 23:28
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Adds a plugin for subscription to direct mail newsletters (collecting subscriptions in the tt_address table)',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.1.0',
+	'version' => '1.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => 1,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 1,
+	'clearcacheonload' => 0,
 	'lockType' => '',
 	'author' => 'Ivan Kartolo',
 	'author_email' => 'ivan.kartolo@dkd.de',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:12:"ext_icon.gif";s:4:"8d58";s:14:"ext_tables.php";s:4:"f6c1";s:28:"ext_typoscript_constants.txt";s:4:"3daf";s:24:"ext_typoscript_setup.txt";s:4:"86bb";s:13:"locallang.php";s:4:"1ab3";s:27:"pi/class.dmailsubscribe.php";s:4:"650e";s:30:"pi/fe_admin_dmailsubscrip.tmpl";s:4:"495f";s:33:"pi/fe_admin_dmailsubscrip_dk.tmpl";s:4:"862b";}',
+	'_md5_values_when_last_written' => 'a:9:{s:30:"direct_mail_subscription.patch";s:4:"cd71";s:12:"ext_icon.gif";s:4:"8d58";s:14:"ext_tables.php";s:4:"f6c1";s:28:"ext_typoscript_constants.txt";s:4:"3daf";s:24:"ext_typoscript_setup.txt";s:4:"86bb";s:13:"locallang.php";s:4:"1ab3";s:27:"pi/class.dmailsubscribe.php";s:4:"5ba4";s:30:"pi/fe_admin_dmailsubscrip.tmpl";s:4:"495f";s:33:"pi/fe_admin_dmailsubscrip_dk.tmpl";s:4:"862b";}',
 );
 
 ?>
