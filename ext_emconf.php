0c2847afa2b27473bc85c23383d90a963cd712f9
ivankartolo <ivankartolo@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-01 11:02:04 +0000

    * Release 1.2.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_subscription/trunk@53598 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "direct_mail_subscription".
 #
-# Auto generated 06-10-2011 23:28
+# Auto generated 01-11-2011 11:37
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Adds a plugin for subscription to direct mail newsletters (collecting subscriptions in the tt_address table)',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.1.1',
+	'version' => '1.2.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:30:"direct_mail_subscription.patch";s:4:"cd71";s:12:"ext_icon.gif";s:4:"8d58";s:14:"ext_tables.php";s:4:"f6c1";s:28:"ext_typoscript_constants.txt";s:4:"3daf";s:24:"ext_typoscript_setup.txt";s:4:"86bb";s:13:"locallang.php";s:4:"1ab3";s:27:"pi/class.dmailsubscribe.php";s:4:"5ba4";s:30:"pi/fe_admin_dmailsubscrip.tmpl";s:4:"495f";s:33:"pi/fe_admin_dmailsubscrip_dk.tmpl";s:4:"862b";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"07db";s:10:"README.txt";s:4:"e4af";s:12:"ext_icon.gif";s:4:"8d58";s:14:"ext_tables.php";s:4:"8517";s:14:"ext_tables.sql";s:4:"1e8e";s:15:"fe_adminLib.inc";s:4:"6f3d";s:13:"locallang.php";s:4:"1ab3";s:16:"locallang_db.xml";s:4:"f6d4";s:27:"pi/class.dmailsubscribe.php";s:4:"44f7";s:30:"pi/fe_admin_dmailsubscrip.tmpl";s:4:"1bac";s:16:"pi/locallang.xml";s:4:"c842";s:20:"static/constants.txt";s:4:"3daf";s:16:"static/setup.txt";s:4:"caed";}',
+	'suggests' => array(
+	),
 );
 
 ?>
