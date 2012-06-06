63ae9adcab8dc57ff92c2db1bdfd1c6499d393cd
ivankartolo <ivankartolo@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-06 16:36:32 +0000

    Release Version 2.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail_subscription/trunk@63213 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "direct_mail_subscription".
 #
-# Auto generated 01-11-2011 11:37
+# Auto generated 06-06-2012 18:34
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,8 +15,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Adds a plugin for subscription to direct mail newsletters (collecting subscriptions in the tt_address table)',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.2.0',
-	'dependencies' => '',
+	'version' => '2.0.0',
+	'dependencies' => 'tt_address',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"07db";s:10:"README.txt";s:4:"e4af";s:12:"ext_icon.gif";s:4:"8d58";s:14:"ext_tables.php";s:4:"8517";s:14:"ext_tables.sql";s:4:"1e8e";s:15:"fe_adminLib.inc";s:4:"6f3d";s:13:"locallang.php";s:4:"1ab3";s:16:"locallang_db.xml";s:4:"f6d4";s:27:"pi/class.dmailsubscribe.php";s:4:"44f7";s:30:"pi/fe_admin_dmailsubscrip.tmpl";s:4:"1bac";s:16:"pi/locallang.xml";s:4:"c842";s:20:"static/constants.txt";s:4:"3daf";s:16:"static/setup.txt";s:4:"caed";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"2578";s:10:"README.txt";s:4:"e4af";s:12:"ext_icon.gif";s:4:"8d58";s:14:"ext_tables.php";s:4:"8517";s:14:"ext_tables.sql";s:4:"1e8e";s:15:"fe_adminLib.inc";s:4:"a5f6";s:13:"locallang.php";s:4:"1ab3";s:16:"locallang_db.xml";s:4:"f6d4";s:27:"pi/class.dmailsubscribe.php";s:4:"44f7";s:30:"pi/fe_admin_dmailsubscrip.tmpl";s:4:"a4dd";s:16:"pi/locallang.xml";s:4:"c842";s:20:"static/constants.txt";s:4:"3daf";s:16:"static/setup.txt";s:4:"e9a2";}',
 	'suggests' => array(
 	),
 );
