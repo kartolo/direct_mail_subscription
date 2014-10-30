9df8c4af4ec8271894cfbd3bcf35d1c0e93fc8a2
Ivan Kartolo <ivan.kartolo@gmail.com>
2014-10-30 16:37:16 +0100

    [TASK] Sync TER and Git
    sync the last version from TER into Git.
    
    Resolves: #62566
    Releases: 2.0
    Change-Id: Ia11fec2c71be69d439a04ed928b1049b03ce0fac
    Reviewed-on: http://review.typo3.org/33622
    Reviewed-by: Ivan Dharma Kartolo <ivan.kartolo@gmail.com>
    Tested-by: Ivan Dharma Kartolo <ivan.kartolo@gmail.com>

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,22 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "direct_mail_subscription".
-#
-# Auto generated 06-06-2012 18:34
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "direct_mail_subscription".
+ *
+ * Auto generated 30-10-2014 16:15
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Direct Mail Subscription',
 	'description' => 'Adds a plugin for subscription to direct mail newsletters (collecting subscriptions in the tt_address table)',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '2.0.0',
-	'dependencies' => 'tt_address',
+	'version' => '2.0.1',
+	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -35,16 +35,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'tt_address' => '',
-			'typo3' => '4.5.0-0.0.0',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"2578";s:10:"README.txt";s:4:"e4af";s:12:"ext_icon.gif";s:4:"8d58";s:14:"ext_tables.php";s:4:"8517";s:14:"ext_tables.sql";s:4:"1e8e";s:15:"fe_adminLib.inc";s:4:"a5f6";s:13:"locallang.php";s:4:"1ab3";s:16:"locallang_db.xml";s:4:"f6d4";s:27:"pi/class.dmailsubscribe.php";s:4:"44f7";s:30:"pi/fe_admin_dmailsubscrip.tmpl";s:4:"a4dd";s:16:"pi/locallang.xml";s:4:"c842";s:20:"static/constants.txt";s:4:"3daf";s:16:"static/setup.txt";s:4:"e9a2";}',
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"2578";s:12:"ext_icon.gif";s:4:"8d58";s:14:"ext_tables.php";s:4:"8517";s:14:"ext_tables.sql";s:4:"1e8e";s:15:"fe_adminLib.inc";s:4:"715e";s:13:"locallang.php";s:4:"1ab3";s:16:"locallang_db.xml";s:4:"f6d4";s:10:"README.txt";s:4:"e4af";s:27:"pi/class.dmailsubscribe.php";s:4:"44f7";s:30:"pi/fe_admin_dmailsubscrip.tmpl";s:4:"a4dd";s:16:"pi/locallang.xml";s:4:"c842";s:20:"static/constants.txt";s:4:"3daf";s:16:"static/setup.txt";s:4:"e9a2";}',
 );
 
 ?>
