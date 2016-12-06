45034bc393c3965cda1a044de8c761c645c86fcd
Cedric Ziel <cedric@cedric-ziel.com>
2016-12-06 10:49:35 +0100

    [BUGFIX] Add common fields to feAdmin editable field list
    Fixes: #9

--- Configuration/TCA/Overrides/tt_address.php
+++ Configuration/TCA/Overrides/tt_address.php
@@ -3,4 +3,4 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TCA']['tt_address']['feInterface']['fe_admin_fieldList'] .= ',name';
+$GLOBALS['TCA']['tt_address']['feInterface']['fe_admin_fieldList'] .= ',hidden,gender,name,email,first_name,last_name,company';
